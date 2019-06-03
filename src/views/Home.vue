bd63a7b016bbc0648a63ac4ef458bd65e8d9b7cd
Natalia <ncbarra@uc.cl>
2019-06-03 12:28:16 -0400

    Add datalist with chilean cities in alphabetical order

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,26 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" v-model="city" list="cities">
+    <datalist id="cities">
+      <option v-for="city in cities" :key="city.id">{{ city.name }}</option>
+    </datalist>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import cityList from "@/chileanCities.json";
 
 export default {
   name: "home",
+  data() {
+    return {
+      city: "",
+      cities: cityList
+    };
+  },
   components: {
     HelloWorld
   }
